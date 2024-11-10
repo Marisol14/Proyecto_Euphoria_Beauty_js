dcea0405a71de42549e43eadfe1609c82813f621
Marisol14 <marisolalcaraz0014@gmail.com>
2024-11-09 22:19:53 -0300

    euphoria-beauty_preentrega3

---
+++ js/productos.js
@@ -0,0 +1,47 @@
+const stockProductos = [
+    {
+        id:1,
+        nombre:"Labial retráctil", 
+        precio: 10000,
+    },
+    {
+        id: 2,
+        nombre:"Labial líquido",
+        precio: 15000,
+    },
+    {
+        id: 3,
+        nombre:"Labial" ,
+        precio: 20000,
+    },
+    {
+        id: 4,
+        nombre: "Sombra de ojos" ,
+        precio: 30000,
+    },
+    {
+        id: 5,
+        nombre: "Máscara de pestañas",
+        precio: 20000,
+    },
+    {
+        id: 6,
+        nombre:"Delineador",
+        precio: 10000,
+    },
+    {
+        id: 7,
+        nombre: "Base de maquillaje",
+        precio: 25000,
+    },
+    {
+        id: 8,
+        nombre: "Corrector",
+        precio: 16000,
+    },
+    {
+        id: 9,
+        nombre: "Rubor",
+        precio: 14000,
+    },
+]
